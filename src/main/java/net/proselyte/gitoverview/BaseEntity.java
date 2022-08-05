fb657d90ff64708a3a61c1c5f1e2e378c4c35ba4
Ruslan Agakisiyev <agakisiyevruslan78@gmail.com>
2022-08-05 12:02:22 +0400

    Adding New class BaseEntity

---
+++ src/main/java/net/proselyte/gitoverview/BaseEntity.java
@@ -0,0 +1,21 @@
+package net.proselyte.gitoverview;
+
+public class BaseEntity {
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseEntity{" +
+                "id=" + id +
+                '}';
+    }
+
+}
